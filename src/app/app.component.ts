77863d737e83183243a04f139523c5a450cd782b
Mahdi Nikzad <mahdi.nikzad@npa.gov.af>
2020-09-17 11:46:49 +0430

    finishes initial design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,9 +17,8 @@ export class AppComponent {
 
   expandList(option: IOption) {
     this.data.forEach(item => {
-      if (item.name === option.name) {
-        item.is_expanded = true;
-      }
+      item.name === option.name ?
+        item.is_expanded = true : item.is_expanded = false;
     })
   }
 }
@@ -28,6 +27,7 @@ export class AppComponent {
 interface IOption {
   name: string;
   icon: string;
+  link: string;
   children?: IOption[];
   is_expanded?: boolean;
 }
@@ -36,64 +36,73 @@ const TREE_DATA: IOption[] = [
   {
     name: 'Orders & bills',
     icon: 'print',
+    link: '/orders',
     children: [
-      {name: 'History', icon: 'print'},
-      {name: 'Tables', icon: 'print'},
-      {name: 'Menu categories', icon: 'print'},
-      {name: 'Menu items', icon: 'print'},
+      {name: 'History', icon: 'print', link: '/orders/history'},
+      {name: 'Tables', icon: 'print', link: '/orders/tables'},
+      {name: 'Menu categories', icon: 'print', link: '/orders/menuCategories'},
+      {name: 'Menu items', icon: 'print', link: '/orders/menuItems'},
     ]
   },
   {
     name: 'Providers',
-    icon: 'local_cafe'
+    icon: 'local_cafe',
+    link: '/providers'
   },
   {
     name: 'Expenses',
     icon: 'monetization_on',
+    link: '/expenses'
   },
   {
     name: 'Stock',
     icon: 'store',
+    link: '/stock',
     children: [
-      {name: 'Manual usage', icon: 'store'},
-      {name: 'Add / correction log', icon: 'store'},
-      {name: 'Usage log', icon: 'store'},
+      {name: 'Manual usage', icon: 'store', link: '/stock/manualUsage'},
+      {name: 'Add / correction log', icon: 'store', link: '/stock/addCorrectionLog'},
+      {name: 'Usage log', icon: 'store', link: '/stock/usageLog'},
     ]
   },
   {
     name: 'Employees',
     icon: 'people',
+    link: '/employees',
     children: [
-      {name: 'Leaves and absenties', icon: 'people'},
-      {name: 'Advances', icon: 'people'},
-      {name: 'Payroll', icon: 'people'}
+      {name: 'Leaves and absenties', icon: 'people', link: '/employees/leavesAndAbsenties'},
+      {name: 'Advances', icon: 'people', link: '/employees/advances'},
+      {name: 'Payroll', icon: 'people', link: '/employees/payroll'}
     ]
   },
   {
     name: 'Reports',
     icon: 'report',
+    link: '/reports',
     children: [
-      {name: 'Payments log', icon: 'report'},
-      {name: 'Daily summary report', icon: 'report'},
-      {name: 'Items summary report', icon: 'report'},
-      {name: 'Categories summary report', icon: 'report'},
-      {name: 'Debts', icon: 'report'}
+      {name: 'Payments log', icon: 'report', link: '/reports/paymentsLog'},
+      {name: 'Daily summary report', icon: 'report', link: '/reports/dailySummaryReport'},
+      {name: 'Items summary report', icon: 'report', link: '/reports/itemsSummaryReport'},
+      {name: 'Categories summary report', icon: 'report', link: '/reports/categoriesSummaryReport'},
+      {name: 'Debts', icon: 'report', link: '/reports/debts'}
     ]
   },
   {
     name: 'Settings',
     icon: 'settings',
+    link: '/settings',
     children: [
-      {name: 'Users', icon: 'settings'}
+      {name: 'Users', icon: 'settings', link: '/settings/users'}
     ]
   },
   {
     name: 'Notifications',
-    icon: 'notifications'
+    icon: 'notifications',
+    link: '/notifications'
   },
   {
     name: 'Logout',
-    icon: 'power_settings_new'
+    icon: 'power_settings_new',
+    link: ''
   },
 
 ];
